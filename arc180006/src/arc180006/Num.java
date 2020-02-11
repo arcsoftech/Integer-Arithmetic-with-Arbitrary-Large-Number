06ed808b05c8ac2f1fc2005069abc62e2b03be5b
arcsoftech <arihant_Chhajed@yahoo.in>
2020-02-11 11:18:30 -0600

    implemented add and subtract method

--- arc180006/src/arc180006/Num.java
+++ arc180006/src/arc180006/Num.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class Num implements Comparable<Num> {
 
     static long defaultBase = 10; // Change as needed
-    static long base = defaultBase; // Change as needed
+    static long base = 100000; // Change as needed
     long[] arr; // array to store arbitrarily large integers
     boolean isNegative; // boolean flag to represent negative numbers
     static boolean karatsuba = false;
@@ -28,15 +28,14 @@ public class Num implements Comparable<Num> {
             throw new NullPointerException("Invalid number");
         }
         list = new ArrayList<>();
-        Num base10 = new Num(10L);
+        Num Base = new Num(base);
         char[] arr = new StringBuilder(s).toString().toCharArray();
         Num num = this;
         for (char current : arr) {
             if (current == '-') {
                 this.isNegative = true;
-            } 
-            else {
-                Num first = Num.product(num, base10);
+            } else {
+                Num first = Num.product(num, Base);
                 Num second = new Num(current - '0');
                 num = Num.add(first, second);
             }
@@ -47,7 +46,7 @@ public class Num implements Comparable<Num> {
     public Num(long x) {
         list = new LinkedList<>();
         long quotient = base + 1, remainder = 0;
-        if(x<0){
+        if (x < 0) {
             this.isNegative = true;
             x = Math.abs(x);
         }
@@ -59,7 +58,7 @@ public class Num implements Comparable<Num> {
                 remainder = x % base;
                 list.add(remainder);
                 x = quotient;
-            } 
+            }
             if (quotient != 0) {
                 list.add(quotient);
             }
@@ -67,23 +66,45 @@ public class Num implements Comparable<Num> {
     }
 
     public static Num add(Num a, Num b) {
-        return null;
+        Num out = null;
+        if (a.isNegative && !b.isNegative) {
+            out = differenceHelper(b, a);
+        } else if (!a.isNegative && b.isNegative) {
+            out = differenceHelper(a, b);
+        } else if (a.isNegative && b.isNegative) {
+            out = addHelper(b, a);
+            out.isNegative = true;
+        } else {
+            out = addHelper(a, b);
+        }
+        return out;
     }
 
     public static Num subtract(Num a, Num b) {
-        return null;
+        Num out = null;
+        if (a.isNegative && !b.isNegative) {
+            out = addHelper(a, b);
+            out.isNegative = true;
+        } else if (!a.isNegative && b.isNegative) {
+            out = addHelper(a, b);
+        } else if (a.isNegative && b.isNegative) {
+            out = differenceHelper(b, a);
+        } else {
+            out = differenceHelper(a, b);
+        }
+        return out;
     }
 
     public static Num product(Num a, Num b) {
         Num out = null;
-        if(a.getList().size() == 0 || b.getList().size() == 0){
+        if (a.getList().size() == 0 || b.getList().size() == 0) {
             return new Num(0L);
         }
-        if(karatsuba){
-            //karatsuba implementation
+        if (karatsuba) {
+            // karatsuba implementation
 
-        }else{
-           // Normal multiplication
+        } else {
+            // Normal multiplication
         }
 
         out.isNegative = a.isNegative ^ b.isNegative;
@@ -153,6 +174,7 @@ public class Num implements Comparable<Num> {
     public static Num evaluateInfix(String[] expr) {
         return null;
     }
+
     // Parse/evaluate an expression in infix and return resulting number
     // Input expression is a string, e.g., "(3 + 4) * 5"
     // Tokenize the string and then input them to parser
@@ -160,18 +182,110 @@ public class Num implements Comparable<Num> {
     public static Num evaluateExp(String expr) {
         return null;
     }
+
     public List<Long> getList() {
         return list;
     }
 
+    private static Num differenceHelper(Num a, Num b) {
+        Num out = new Num();
+        int compareOut = compareList(a.getList(), b.getList());
+        if (compareOut > 0) {
+            subtract(a.getList(), b.getList(),out.getList());
+        } else if (compareOut < 0) {
+            subtract(b.getList(), a.getList(),out.getList());
+            out.isNegative = true;
+        } else {
+            out = new Num(0L);
+        }
+        return out;
+    }
+
+    private static Num addHelper(Num a, Num b) {
+        Num out = new Num();
+        add(a.getList(), b.getList(), out.getList(), Num.base);
+        return out;
+    }
+
+    private static Long next(Iterator<Long> iterator){
+        return iterator.hasNext()? iterator.next() : 0L;
+    }
+    
+    private static void add(List<Long> num1List, List<Long> num2List, List<Long> out, long base) {
+        
+        Iterator<Long> num1Iterator = num1List.iterator();
+    	Iterator<Long> num2Iterator = num2List.iterator();
+    	Long carry = 0L;
+    	while(num1Iterator.hasNext() || num2Iterator.hasNext() || carry > 0){
+            Long sum = next(num1Iterator) + next(num2Iterator) + carry;
+            if (sum >= base) {
+                carry = 1L;
+                sum -= base;
+            }
+            else {
+                carry = 0L;
+            }
+            out.add(sum);
+    	}
+
+    }
+
+    private static void subtract(List<Long> num1List, List<Long> num2List, List<Long> outList) {
+        Iterator<Long> num1Iterator = num1List.iterator();
+        Iterator<Long> num2Iterator = num2List.iterator();
+        Long borrow = 0L;
+        while(num1Iterator.hasNext() || num2Iterator.hasNext() || borrow > 0){
+            Long a = next(num1Iterator);
+            Long b = next(num2Iterator);
+            Long out;
+            if(a>=b)
+            {
+                out= a-(b+borrow);
+                borrow = 0L;
+            }
+            else
+            {
+                out = (a+base) - b; 
+                borrow = 1L;
+            }
+            outList.add(out);
+
+    	}
+        
+    }
+
+    private static int compareList(List<Long> num1List, List<Long> num2List) {
+        int out = 0;
+        Iterator<Long> iterator1 = num1List.iterator();
+        Iterator<Long> iterator2 = num2List.iterator();
+        while (iterator1.hasNext() || iterator2.hasNext()) {
+            Long a = iterator1.hasNext() ? iterator1.next() : 0L;
+            Long b = iterator2.hasNext() ? iterator2.next() : 0L;
+            if (a > b) {
+                out = 1;
+            } else if (a < b) {
+                out = -1;
+            }
+        }
+        return out;
+    }
+
+    /**
+     * Main Function- Driver Code
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         Num x = new Num(999);
-        Num y = new Num("80");
+        Num y = new Num(80);
         Num z = Num.add(x, y);
+        Num d = Num.subtract(x, y);
+        System.out.println(d);
         System.out.println(z);
         Num a = Num.power(x, 8);
         System.out.println(a);
         if (z != null)
             z.printList();
     }
+
 }
